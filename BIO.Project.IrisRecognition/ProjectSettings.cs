dd3606e95b8f8c1a131cb3115027c3ac1739dd7f
archie <archie@DESKTOP-H2CJHNT>
2016-11-28 12:22:25 -0800

    project settings

--- BIO.Project.IrisRecognition/ProjectSettings.cs
+++ BIO.Project.IrisRecognition/ProjectSettings.cs
@@ -18,7 +18,7 @@ namespace BIO.Project.IrisRecognition
         }
 
         public override Framework.Core.Database.IDatabaseCreator<StandardRecord<StandardRecordData>> getDatabaseCreator() {
-            return new IrisDatabaseCreator(@"d:\db\face\2D\JAFFE"); // !!!!!!!!!!!!! PATH TO PICTURES FOLDER !!!!!!!!!!!!!!!!!!!!!
+            return new IrisDatabaseCreator(@"C:\Users\archie\Desktop\CASIA-IrisV1"); // !!!!!!!!!!!!! PATH TO PICTURES FOLDER !!!!!!!!!!!!!!!!!!!!!
         }
 
         protected override Framework.Core.Evaluation.Block.IBlockEvaluatorSettings<StandardRecord<StandardRecordData>, EmguGrayImageInputData> getEvaluatorSettings() {
